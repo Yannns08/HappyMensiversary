8058907ca968ee8f6e2901b32ad961d00a386475
Yannns08 <162995023+Yannns08@users.noreply.github.com>
2024-03-16 13:14:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ button.addEventListener("click", function(){
 function progressInterva(){
     if(count == 100 && percentWidth == 100){
         clearInterval(progressLoad)
-        percent.textContent = "Ok rùi đó:)";
+        percent.textContent = "Makasih yah udah nunggu❤️";
         percent.style.letterSpacing = "1px";
         textBox.style.transform = "scale(1.3)"
         heartItem3.style.animation = "1s heartScale forwards"
